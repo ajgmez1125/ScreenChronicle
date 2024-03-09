f21b533c0c873c6ac468a8956709b67c2c943442
Ayden <ajgg1125@gmail.com>
2024-03-08 23:29:23 -0500

    fixed migration issue, if you have issues on ur end pls contact

--- Infrastructure/src/main/resources/db/migration/V5__dummy_data.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO user VALUES (1, 'user123', 'password123', 'email@email.com');
-
-INSERT INTO user VALUES (2, 'ayden', 'password123', 'email@cool.com');
-
-INSERT INTO review VALUES (1, 2, 1, 'This movie is great!', 5);
-
-INSERT INTO review VALUES (2,2,2, 'This movie isn that good', 2);
-
-INSERT INTO review VALUES (3,1,2,'Its ok', 2);
